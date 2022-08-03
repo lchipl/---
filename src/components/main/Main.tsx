3b89279ee4c1763ae0e574481556ff79d4af9c6f
19122177 <evchip@sberbank.ru>
2022-08-03 09:38:18 +0300

    стили под ссылки

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -5,24 +5,25 @@ import "../../styles/styles.scss";
 export const Main = () => {
     return (
         <main className="main">
-            <h1>Услуги и сервисы</h1>
+            <h1 className="title">Услуги и сервисы</h1>
             <nav className="navigation">
                 <ul className="navigation__list">
-                    <li className="navigation__item">
-                        <a href="#">Ремонтируемые бренды</a></li>
-                    <li className="navigation__item"><a href="#">Ремонтируемые устройства</a></li>
-                    <li className="navigation__item"><a href="#">Цены на услуги</a></li>
-                    <li className="navigation__item"><a href="#">Адреса сервисных центров</a></li>
-                    <li className="navigation__item"><a href="#">Специальные цены</a></li>
-                    <li className="navigation__item"><a href="#">Отзывы</a></li>
+                    <li className="navigation__item"><a className="navigation__link" href="#">Ремонтируемые бренды</a></li>
+                    <li className="navigation__item"><a className="navigation__link" href="#">Ремонтируемые устройства</a></li>
+                    <li className="navigation__item"><a className="navigation__link" href="#">Цены на услуги</a></li>
+                    <li className="navigation__item"><a className="navigation__link" href="#">Адреса сервисных центров</a></li>
+                    <li className="navigation__item"><a className="navigation__link" href="#">Специальные цены</a></li>
+                    <li className="navigation__item"><a className="navigation__link" href="#">Отзывы</a></li>
                 </ul>
             </nav>
-            <img src={entryIcon} alt="doorWiev" />
-            <p>Мы являемся авторизованным сервисным центром по ремонту техники Dell.
+            <p className="text">Мы являемся авторизованным сервисным центром по ремонту техники Dell.
                 Только у нас вы можете отремонтировать свой ноутбук Dell с официальной гарантией производителя.
                 Мы успешно работаем с 1992 года и заслужили репутацию надежного партнера
             </p>
-            <button>Читать далее</button>
+            <div className="readmore">
+                <button className="readmore__link">Читать далее</button>
+            </div>
+            <img src={entryIcon} alt="doorWiev" />
         </main>
     )
 }
