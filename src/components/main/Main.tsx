eef975efed9cceae1de254656f440314b02043af
19122177 <evchip@sberbank.ru>
2022-08-08 15:12:27 +0300

    доработана верстка по макету, подключенные шрифты

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,20 +1,22 @@
 import React from "react";
-import entryIcon from "../../assets/entry.svg";
+import entryIcon from "../../assets/entry.jpg";
 import "../../styles/styles.scss";
 
 export const Main = () => {
     return (
         <main className="main">
-            <h1 className="title">Услуги и сервисы</h1>
             <div className="services">
-                <button className="services__button">
-                    <p className="services__label">Оставить заявку</p>
-                    <div className="button icon-settings"></div>
-                </button>
-                <button className="services__button">
-                    <p className="services__label">Статус ремонта</p>
-                    <div className="button icon-search"></div>
-                </button>
+                <h1 className="title services__title">Услуги и сервисы</h1>
+                <div className="services__buttons">
+                    <button className="services__button">
+                        <p className="services__label">Оставить заявку</p>
+                        <div className="button icon-settings"></div>
+                    </button>
+                    <button className="services__button">
+                        <p className="services__label">Статус ремонта</p>
+                        <div className="button icon-search"></div>
+                    </button>
+                </div>
             </div>
             <nav className="navigation">
                 <ul className="navigation__list">
@@ -26,16 +28,24 @@ export const Main = () => {
                     <li className="navigation__item"><a className="navigation__link" href="#">Отзывы</a></li>
                 </ul>
             </nav>
-            <div>
-                <p className="text">Мы являемся авторизованным сервисным центром по ремонту техники Dell.
-                    Только у нас вы можете отремонтировать свой ноутбук Dell с официальной гарантией производителя.
-                    Мы успешно работаем с 1992 года и заслужили репутацию надежного партнера
+            <div className="about">
+                <div className="about__description">
+                <p className="text">
+                    Мы являемся авторизованным сервисным центром по ремонту техники Dell. Только у нас
+                    вы можете отремонтировать свой ноутбук Dell с официальной гарантией производителя.
                 </p>
-            </div>
-            <div className="readmore">
-                <button className="readmore__button">Читать далее</button>
+                <br/>
+                <p className="text">
+                    Мы успешно работаем с 1992 года и заслужили репутацию надежного партнера, что
+                    подтверждает большое количество постоянных клиентов. Мы гордимся тем, что к нам
+                    обращаются по рекомендациям и, в свою очередь, советуют нас родным и близким.
+                </p>
+                <div className="readmore">
+                    <button className="readmore__button">Читать далее</button>
+                </div>
             </div>
             <img className="about__img" src={entryIcon} alt="logo" />
+            </div>
         </main>
     )
 }
