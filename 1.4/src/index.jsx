18e806a6a69187c9f4bf59235d60ef91841b9273
19122177 <evchip@sberbank.ru>
2022-08-12 17:22:53 +0300

    1.4 добавлены бэм классы

--- 1.4/src/index.jsx
+++ 1.4/src/index.jsx
@@ -4,34 +4,38 @@ import {render} from "react-dom";
 const App = () => {
     return (
         <aside>
-            <header>
-                 закрыть
-                главный лейбл
-                поиск
+            <header className="sidebar__header">
+                        <button className="button icon-close"></button>
+                        <a href="#"><img src="../../assets/logo.svg" alt="logo" className="logo"/></a>
+                        <div className="button icon-search-sidebar"></div>
             </header>
-            <div>
-                <ul>
-                    <li>Ремонт техники</li>
-                    <li>Услуги и сервисы</li>
-                    <li>Корпоративным клиентам</li>
-                    <li>Продавцам техники</li>
-                    <li>Партнерам</li>
-                    <li>Производителям</li>
-                    <li>Наши сервисные центры</li>
-                    <li>Контакты</li>
+            <nav>
+                <ul className="menu">
+                    <li><a href="#" className="menu__link menu__link--active">Ремонт техники</a></li>
+                    <li><a href="#" className="menu__link">Услуги и сервисы</a></li>
+                    <li><a href="#" className="menu__link">Корпоративным клиентам</a></li>
+                    <li><a href="#" className="menu__link">Продавцам техники</a></li>
+                    <li><a href="#" className="menu__link">Партнерам</a></li>
+                    <li><a href="#" className="menu__link">Производителям</a></li>
+                    <li><a href="#" className="menu__link">Наши сервисные центры</a></li>
+                    <li><a href="#" className="menu__link">Контанты</a></li>
                 </ul>
-            </div>
+            </nav>
             <footer>
-                <div>
-                    3 иконки
-                </div>
+                <div className="contacts">
                 <a href="mailto:mail@cps.com">mail@cps.com </a>
-                <a href="tel:88008908900">8 800 890 8900</a>
+                <br/>
+                    <div className="contacts__buttons-row">
+                    <a href="tel:88008908900">8 800 890 8900</a>
+                    <button className="button icon-profile"></button>
+                    </div>
+                </div>
                 <div>
-                    langs
-                    <input type="radio" />
-                    <input type="radio" />
-                    <input type="radio" />
+                    <div className="languages">
+                        <button className="languages__button languages__button--active">RU</button>
+                        <button className="languages__button">EN</button>
+                        <button className="languages__button">CH</button>
+                    </div>
                 </div>
             </footer>
         </aside>
